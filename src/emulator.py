b2091cad043fe656cfdc217145803d08f0cda533
JohnSilverman <52631852+JohnSilverman@users.noreply.github.com>
2021-06-05 03:06:14 +0900

    dd

--- src/emulator.py
+++ src/emulator.py
@@ -8,45 +8,154 @@ def print_error(code,debug_data):
 		print("에러! 이미 선언된 변수를 재선언합니다 :", debug_data)
 	else:
 		print("에러!",debug_data)
+	exit(-1)
 
 mem = {} #변수 저장하는 곳
+
+def STMTS(node):
+	if node.children[0].token[0]=="eps":
+		return
+	else:
+		stmt_node = node.children[0]
+		toktype = stmt_node.token[0]
+		if toktype == "IfStmt":
+			IFSTMT(stmt_node)
+		elif toktype =="LetStmt":
+			LETSTMT(stmt_node)
+		elif toktype =="IOStmt":
+			IOSTMT(stmt_node)
+		elif toktype == "AssignStmt":
+			ASSIGN(stmt_node)
+
+	STMTS(node.children[1])
+
+def ASSIGN(node):
+	name = node.children[0].token[1]
+	eval = EXP(node.children[2])
+	if name not in mem:
+		print_error(0,name)
+	mem[name] = eval
+
+def IFSTMT(node):
+	eval = EXP(node.children[2])
+	if eval!=0:
+		STMTS(node.children[5])
+	else:
+		ELSESTMT(node.children[7])
+
+def ELSESTMT(node):
+	if node.children[0].token[0]=="eps":
+		return
+	
+	STMTS(node.children[2])
+
 
 def LETSTMT(node):
 	name = node.children[1].token[1]
 	if name in mem:
 		print_error(1,name)
-		return -1
 	else:
 		mem[name] = 0
 	
-	# if len(node.children)==5:
-	# 	eval = EXP(node.children[3])
-	# 	mem[name] = eval
+	if len(node.children)==5:
+		eval = EXP(node.children[3])
+		mem[name] = eval
 	return 0
 
-def ASSIGN(name,value):
-	if name in mem:
-		mem[name] = value
-	else:
-		print_error(0,name)
-	return 0
+def IOSTMT(node):
+	func = node.children[0].token[0]
+
+	if func == "print":
+		if node.children[2].token[0] == "EXP":
+			print(EXP(node.children[2]))
+		elif node.children[2].token[0] == "String":
+			print(STRING(node.children[2]))
+	elif func == "input":
+		if node.children[4].token[0] == "String":
+			print(STRING(node.children[4]))
+		
+		name = node.children[2].token[1]
+		if name not in mem:
+			print_error(0,name)
+
+		mem[name] = int(input())
+
+def EXP(node):
+	if len(node.children)==1:
+		return TERM(node.children[0])
+
+	op = node.children[1].token[0]
+	term1 = TERM(node.children[0])
+	term2 = TERM(node.children[2])
+
+	if op=="+":
+		return term1+term2
+	elif op=="-":
+		return term1-term2
+
+def TERM(node):
+	if len(node.children)==1:
+		return FACT(node.children[0])
+
+	op = node.children[1].token[0]
+	fact1 = FACT(node.children[0])
+	fact2 = FACT(node.children[2])
+
+	if op=="*":
+		return fact1*fact2
+	elif op=="/":
+		return fact1//fact2
+
+def FACT(node):
+	if len(node.children)==1:
+		return LOGI(node.children[0])
+	
+	op = node.children[1].token[0]
+	logi1 = LOGI(node.children[0])
+	logi2 = LOGI(node.children[2])
+
+	if op==">":
+		return 1 if logi1>logi2 else 0
+	elif op=="<":
+		return 1 if logi1<logi2 else 0
+	elif op==">=":
+		return 1 if logi1>=logi2 else 0
+	elif op=="<=":
+		return 1 if logi1<=logi2 else 0
+	elif op=="==":
+		return 1 if logi1==logi2 else 0
 
 def LOGI(node):
-	token = node.token
-	if token[0]=="ID":
-		if token[1] not in mem:
-			print_error(1,token[1])
-			return -1
-		else:
-			return mem[token[1]]
-	elif token[0]=="Constant":
-		return token[1]
-	#TODO : (EXP) 추가
+	children = node.children
+	if children[0].token[0] == "ID":
+		return ID(children[0])
+	elif children[0].token[0] == "Constant":
+		return CONSTANT(children[0])
+	elif children[1].token[0] == "EXP":
+		return EXP(children[1])
 
-def 
+def ID(node):
+	name = node.token[1]
+	if name not in mem:
+		print_error(1,name)
+	else:
+		return mem[name]
+
+def CONSTANT(node):
+	return node.token[1]
+
+def STRING(node):
+	return node.token[1]
 
 def run(parsetree):
-	pass
+	STMTS(parsetree)
+
+def traverse_tree(node):
+	print(node.token)
+	if not node.children:
+		return
+	for child in node.children:
+		traverse_tree(child)
 
 def main():
 	codefile = open(input("Enter code file path : "),"r",encoding="utf-8")
@@ -54,8 +163,8 @@ def main():
 	codefile.close()
 
 	parse_result = SA.lrparse(tokenlist)
-	res = run(parse_result)
-	print("Program returned with value",res)
+	#traverse_tree(parse_result)
+	run(parse_result)
 
 if __name__=='__main__':
 	main()
